2586b2cf49736cd1e02e712e58cfeb7bd5fe800a
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-08-24 14:53:33 +0900

    [BOJ] 퇴사 / 14501 / 퇴사 / 8주차-삼성 / C++

---
+++ 김채윤/8_14501.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+
+int find(vector<pair<int, int>>& v, vector<int>& vec, int day) {
+    if (day > N) return 0;
+
+    int sum = find(v, vec, day + 1);
+
+    if (day + v[day].first <= N + 1)
+        sum = max(sum, v[day].second + find(v, vec, day + v[day].first));
+
+    return sum;
+}
+
+int main() {
+    cin >> N;
+
+    vector<pair<int, int>> v(N + 1);
+    vector<int> vec(N + 1, 0);
+
+    for (int i = 1; i <= N; i++) cin >> v[i].first >> v[i].second;
+
+    cout << find(v, vec, 1);
+
+    return 0;
+}
